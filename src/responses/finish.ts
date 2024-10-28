5ad58baec6f981aa90e2e2e074e2d98f738540f5
gryzun33 <yakushevao777@gmail.com>
2024-10-28 16:58:21 +0300

    feat: change finish response

--- src/responses/finish.ts
+++ src/responses/finish.ts
@@ -16,15 +16,19 @@ export async function sendFinish(
   const responseJSON = JSON.stringify(responseData);
   const response = getFormattedResponse('finish', responseJSON);
   console.log('response type = finish');
-  await Promise.all([ws.send(response), opponentWs.send(response)]);
+  ws.send(response);
+  console.log('response type = finish');
+  opponentWs.send(response);
 
   // response2
   const sockets = stateManager.getAllSockets();
   const winners: WinnersData = stateManager.getWinners();
   const winnersJSON = JSON.stringify(winners);
   const responseWinners = getFormattedResponse('update_winners', winnersJSON);
-  console.log('response type = update_winners');
-  await Promise.all(sockets.map((socket) => socket.send(responseWinners)));
+  sockets.map((socket) => {
+    console.log('response type = update_winners');
+    socket.send(responseWinners);
+  });
 
   resetGame(winnerId, opponentId, gameId);
 }
